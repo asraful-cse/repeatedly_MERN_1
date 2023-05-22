6558eb3abc2c0f57757e69b3c4cd678f705186d6
asraful196 <asraful.01110@gmail.com>
2023-05-23 01:02:06 +0600

    functionSummary for multiple parameter

---
+++ functionSummary.js
@@ -0,0 +1,17 @@
+function addMultiplePerametter(a, b) {
+  const sum = a + b;
+  return sum;
+}
+
+const first = 1;
+const second = 2;
+const total = addMultiplePerametter(first, second);
+console.log("total", total);
+
+const third = 1;
+const forth = 2;
+const total2 = addMultiplePerametter(third, forth);
+console.log("total2", total2);
+
+const total3 = addMultiplePerametter(total, total2);
+console.log("threeeeeeeeee", total3);
