12e95e0e5f5a4aea826c4bf663b34104a77dc2c0
asraful196 <asraful.01110@gmail.com>
2023-05-29 12:36:41 +0600

    math function practice

---
+++ mathFunction.js
@@ -0,0 +1,14 @@
+const num1 = 25;
+const num2 = 45;
+const gap = num1 - num2;
+console.log(gap); // expected : -20
+
+const finalDefference = Math.abs(gap);
+console.log(finalDefference);
+
+const number1 = 5.5;
+console.log(Math.round(number1)); // 5
+const number2 = -5.5;
+console.log(Math.round(number2)); // -5
+
+console.log(Math.round(5.4), Math.round(-5.6)); // 5 -6
