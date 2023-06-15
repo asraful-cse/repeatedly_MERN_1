76989c79e0707420a7ee08b6f7ba728b38f9dc8f
asraful196 <asraful.01110@gmail.com>
2023-06-15 14:39:35 +0600

    larest number of array

---
+++ maxNumberArray.js
@@ -0,0 +1,17 @@
+function maxNumber(numbers) {
+  let largest = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const index = numbers[i];
+
+    if (index < largest) {
+      largest = index;
+    }
+  }
+  return largest;
+}
+
+const numbers = [1, 2, 3, 4, 15, 6, 7, 8, 9];
+
+const countNumbers = maxNumber(numbers);
+
+console.log(countNumbers);
